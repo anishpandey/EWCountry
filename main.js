b7c851769342330381223d019f306080727dedf7
Anish Kumar <pandey.anish@gmail.com>
2021-02-08 16:41:28 +0530

    Updated to 0.5
    Added country specific Hero images

--- main.js
+++ main.js
@@ -1,40 +1,35 @@
 /*
 (c) Copyright 2020 Akamai Technologies, Inc. Licensed under Apache 2 license.
 
-Version: 0.2
-Purpose:  Modify an HTML streamed response by adding a script before the closing head tag.
-Repo: https://github.com/akamai/edgeworkers-examples/tree/master/hello-world
+Version: 1.1
+Purpose:  Modify an HTML streamed response by replacing a text string two times across the entire response.
+
 */
 
-// Import logging module
-import { logger } from 'log'
+import { ReadableStream, WritableStream } from 'streams';
+import { httpRequest } from 'http-request';
+import { createResponse } from 'create-response';
+import { TextEncoderStream, TextDecoderStream } from 'text-encode-transform';
+import { FindAndReplaceStream } from 'find-replace-stream.js';
+import { logger } from 'log';
 
-export function onClientRequest(request) {
-    // Outputs a message to the X-Akamai-EdgeWorker-onClientRequest-Log header.
-    logger.log("Responding with hello world from the path: %s", request.path)
 
+export function responseProvider (request) {
+  // Get text to be searched for and new replacement text from Property Manager variables in the request object.
+  // const tosearchfor = request.getVariable('PMUSER_EWSEARCH');
+  // const newtext = request.getVariable('PMUSER_EWNEWTEXT');
+  // Set to 0 to replace all, otherwise a number larger than 0 to limit replacements
+  var country = request.userLocation.country;
+  const tosearchfor =  'images/fd4771c7bd7ae663a8cd04299dc930eb458a7e2c1d99b5627243ad8fa4ff25db048c785c18ad4ce9a791ba1af54d8d56d34f1648752e0f3344ead9_1280.jpg';
+  var newtext =  'images/' + country + '.jpg';
+  //const newtext =  'images/IN.jpg';
+  const howManyReplacements = 1;
 
-    var country = request.userLocation.country;
-    if (country === 'FR') {
-       request.respondWith(
-        200, {},
-        '<html><body><h1>Redirect to France 0.4 </h1></body></html>')
-    }
-    else if (country === 'IN') {
-       request.respondWith(
-        200, {},
-        '<html><body><h1>Should redirect to India 0.4 </h1></body></html>')
-    }
-    else if (country === 'US') {
-       request.respondWith(
-        200, {},
-        '<html><body><h1>Should redirect to USA 0.4 </h1></body></html>')
-    }
-}
-
-export function onClientResponse(request, response) {
-    // Outputs a message to the X-Akamai-EdgeWorker-onClientResponse-Log header.
-    logger.log("Adding a header in ClientResponse")
-
-    response.setHeader('EdgeWorkers', 'From Akamai EdgeWorkers 0.4')
+  return httpRequest(`${request.scheme}://${request.host}${request.url}`).then(response => {
+    return createResponse(
+      response.status,
+      response.headers,
+      response.body.pipeThrough(new TextDecoderStream()).pipeThrough(new FindAndReplaceStream(tosearchfor, newtext, howManyReplacements)).pipeThrough(new TextEncoderStream())
+    );
+  });
 }
